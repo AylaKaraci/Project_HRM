76064f133d6be016e0a80ccd3c5ed183370644e1
AylaKaraci <garet.ruby.store@gmail.com>
2020-10-28 12:20:56 +0300

    AssignWorkOrderViewComponent

---
+++ Project_HRM.UI/Controllers/EmployeeWorkOrderListController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project_HRM.BusinessEngine.Contracts;
+
+namespace Project_HRM.UI.Controllers
+{
+    public class EmployeeWorkOrderListController : Controller
+    {
+        private readonly IEmployeeBusinessEngine _employeeBusinessEngine;
+        public IActionResult Index(int pageNumber = 1)
+        {
+            ViewBag.PageNumber = pageNumber;
+            return View();
+        }
+    }
+}
